6527d73532d975118e84c2346b068b5b2b5fd6ec
hayone1 <boluwatife.morawo@stu.cu.edu.ng>
2021-10-06 21:13:09 +0100

    ALMOST DONE

--- js/portfolio.init.js
+++ js/portfolio.init.js
@@ -1,7 +1,7 @@
 //Portfolio filter
 $(window).on('load', function() {
-    var $container = $('.projects-wrapper');
-    var $filter = $('#filter');
+    var $container = $('.certs-wrapper');
+    var $filter = $('#filterCerts');
     $container.isotope({
         filter: '*',
         layoutMode: 'masonry',
@@ -15,6 +15,32 @@ $(window).on('load', function() {
         $filter.find('a').removeClass('active');
         $(this).addClass('active');
         $container.isotope({
+            filter: selector,
+            animationOptions: {
+                animationDuration: 750,
+                easing: 'linear',
+                queue: false,
+            }
+        });
+        return false;
+    });
+
+
+    var $containerProjects = $('.projects-wrapper');
+    var $filterProjects = $('#filterProjects');
+    $containerProjects.isotope({
+        filter: '*',
+        layoutMode: 'masonry',
+        animationOptions: {
+            duration: 750,
+            easing: 'linear'
+        }
+    });
+    $filterProjects.find('a').click(function() {
+        var selector = $(this).attr('data-filter');
+        $filterProjects.find('a').removeClass('active');
+        $(this).addClass('active');
+        $containerProjects.isotope({
             filter: selector,
             animationOptions: {
                 animationDuration: 750,
